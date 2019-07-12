630e377293215a879c2c774e497cb1f6daa44cbb
MathCastro <matheusfk74@gmail.com>
2019-07-12 13:42:38 -0300

    clock mem

--- Uniciclo/pcMais4_tb.vhd
+++ Uniciclo/pcMais4_tb.vhd
@@ -15,13 +15,15 @@ ARCHITECTURE tb_arch OF pcMais4_tb IS
 	component UNICICLO_RV 
 		port (
 				clk :			in std_logic;
-				clr :			in std_logic
+				clr :			in std_logic;
+				clk_mem: 	in std_logic
 				);
 	end component;
 	
 	begin i1 : UNICICLO_RV PORT MAP (
 		clk => clk,
-		clr => clr
+		clr => clr,
+		clk_mem => clk_mem
 	);
 	
 	init : process
@@ -37,9 +39,17 @@ ARCHITECTURE tb_arch OF pcMais4_tb IS
 	clkgen: process
 	begin
 		clk <= '1';
-		wait for 4 ps;
+		wait for 10 ps;
 		clk <= '0';
-		wait for 4 ps;
+		wait for 10 ps;
+	end process;
+	
+	clkgenmem: process
+	begin
+		clk_mem <= '1';
+		wait for 1 ps;
+		clk_mem <= '0';
+		wait for 1 ps;
 	end process;
 	
 end tb_arch; 
